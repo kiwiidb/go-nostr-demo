1fec9806819be06424ccbf6516dd3da34e3e5219
kiwiidb <kwinten.debacker@hotmail.com>
2023-04-12 11:24:36 +0200

    add damus to utils

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 const (
 	KWINTEN_NPUB = "npub13sajvl5ak6cpz4ycesl0e5v869r5sey5pt50l9mcy6uas0fqtpmscth4np"
 	MY_PRIVKEY   = "9f06a97faf15189c4e6d787b7ec4b5878bd2a08729bf82197a2246cc630ed351"
+	DAMUS_RELAY  = "wss://relay.damus.io"
 	SNORT_RELAY  = "wss://relay.snort.social"
 	EDEN_RELAY   = "wss://eden.nostr.land"
 	WINE_RELAY   = "wss://nostr.wine"
