06873cdbb23ffd34a1a9c98bad2218949ad87296
kiwiidb <kwinten.debacker@hotmail.com>
2023-04-12 11:13:55 +0200

    first commit

---
+++ fetch_profile/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"nostrgo/utils"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+)
+
+func main() {
+	relay, err := nostr.RelayConnect(context.Background(), utils.SNORT_RELAY)
+	if err != nil {
+		panic(err)
+	}
+	_, pubkeyHex, err := nip19.Decode(utils.KWINTEN_NPUB)
+	if err != nil {
+		panic(err)
+	}
+	filters := []nostr.Filter{{
+		Kinds:   []int{0},
+		Authors: []string{pubkeyHex.(string)},
+	}}
+	fmt.Println("Starting subscription")
+	sub := relay.Subscribe(context.Background(), filters)
+	for {
+		select {
+		case ev := <-sub.Events:
+			fmt.Println(ev.Content)
+		case <-sub.EndOfStoredEvents:
+			return
+		}
+	}
+}
